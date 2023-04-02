bf3fc7fba88783eff8cbaef92f3ee8d367dd5012
Blotz <f.p.theil@gmail.com>
2023-04-02 14:28:24 +0100

    bump version

--- pixelsorter/__init__.py
+++ pixelsorter/__init__.py
@@ -1,7 +1,7 @@
 
 __name__    = 'Pixel Sorter'
 __doc__     = 'Command line tool for sorting pixels'
-__version__ = '0.1'
+__version__ = '0.1.1'
 __author__  = 'Ferdinand Theil'
 __email__   = 'f.p.theil@proton.me'
 __license__ = 'MIT'
